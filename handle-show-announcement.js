2bd2bbf859c753c19a8abaae7cfa8fdbea0c0f5d
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-08-26 10:54:55 -0700

    add test for `handle-show-announcement.js`

--- handle-show-announcement.js
+++ handle-show-announcement.js
@@ -12,128 +12,137 @@ dayjs.extend(customParseFormat);
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-// Create Octokit constructor with .paginate API and custom user agent
-const MyOctokit = Octokit.plugin(paginateRest).defaults({
-  userAgent: "gr2m-helpdesk",
-});
-const octokit = new MyOctokit({
-  auth: process.env.GITHUB_TOKEN,
-});
-
-// load open issues with the `show` label
-const showIssues = await octokit.paginate("GET /repos/{owner}/{repo}/issues", {
-  owner: "gr2m",
-  repo: "helpdesk",
-  labels: "show",
-  state: "open",
-  per_page: 100,
-});
-
-const currentShowIssue = showIssues.find((issue) => {
-  const dayString = issue.body
-    .match(/📅.*/)
-    .pop()
-    .replace(/📅\s*/, "")
-    .replace(/^\w+, /, "")
-    .trim();
-  const timeString = issue.body
-    .match(/🕐[^(\r\n]+/)
-    .pop()
-    .replace(/🕐\s*/, "")
-    .replace("Pacific Time", "")
-    .trim();
-
-  // workaround: cannot parse "June 3, 2021 1:00pm" but can parse "June 3, 2021 12:00pm"
-  // workaround: cannot set default timezone, so parse the date/time string first, then use `.tz()` with the expected date/time format
-  let timeStringWithoutAmPm = timeString.replace(/(am|pm)\b/, "");
-
-  let hours = parseInt(timeStringWithoutAmPm, 10);
-
-  if (hours < 9) {
-    timeStringWithoutAmPm = timeStringWithoutAmPm.replace(hours, hours + 12);
-  }
-
-  const tmp = dayjs(
-    [dayString, timeStringWithoutAmPm].join(" "),
-    // "MMMM D, YYYY H:mma", // see workaround
-    "MMMM D, YYYY H:mm",
-    true
-  );
-
-  let time = dayjs.tz(tmp.format("YYYY-MM-DD HH:mm"), "America/Los_Angeles");
-
-  const showIsWithinRange =
-    time.subtract(30, "minutes") > dayjs().subtract(25, "minutes") &&
-    time.subtract(30, "minutes") < dayjs().add(25, "minutes");
-  return showIsWithinRange;
-});
-
-if (!currentShowIssue) {
-  core.setFailed("No current issue found to comment on");
-  process.exit(1);
+if (process.env.GITHUB_ACTIONS && process.env.NODE_ENV !== "test") {
+  // Create Octokit constructor with .paginate API and custom user agent
+  const MyOctokit = Octokit.plugin(paginateRest).defaults({
+    userAgent: "gr2m-helpdesk",
+  });
+  const octokit = new MyOctokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+  run(process.env, core, octokit, twitterRequest);
 }
 
-const [, , title, , guest] = currentShowIssue.title.split(/ (- |with @)/g);
+export async function run(env, core, octokit, twitterRequest) {
+  // load open issues with the `show` label
+  const showIssues = await octokit.paginate(
+    "GET /repos/{owner}/{repo}/issues",
+    {
+      owner: "gr2m",
+      repo: "helpdesk",
+      labels: "show",
+      state: "open",
+      per_page: 100,
+    }
+  );
 
-const currentShow = {
-  title,
-  number: currentShowIssue.number,
-  issue: currentShowIssue,
-  guest,
-  url: currentShowIssue.html_url,
-};
+  const currentShowIssue = showIssues.find((issue) => {
+    const dayString = issue.body
+      .match(/📅.*/)
+      .pop()
+      .replace(/📅\s*/, "")
+      .replace(/^\w+, /, "")
+      .trim();
+    const timeString = issue.body
+      .match(/🕐[^(\r\n]+/)
+      .pop()
+      .replace(/🕐\s*/, "")
+      .replace("Pacific Time", "")
+      .trim();
 
-// add comment on issue
-const {
-  data: { html_url: commentUrl },
-} = await octokit.request(
-  "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
-  {
-    owner: "gr2m",
-    repo: "helpdesk",
-    issue_number: currentShow.number,
-    body: "Going live in 30 minutes at https://twitch.tv/gregorcodes",
+    // workaround: cannot parse "June 3, 2021 1:00pm" but can parse "June 3, 2021 12:00pm"
+    // workaround: cannot set default timezone, so parse the date/time string first, then use `.tz()` with the expected date/time format
+    let timeStringWithoutAmPm = timeString.replace(/(am|pm)\b/, "");
+
+    let hours = parseInt(timeStringWithoutAmPm, 10);
+
+    if (hours < 9) {
+      timeStringWithoutAmPm = timeStringWithoutAmPm.replace(hours, hours + 12);
+    }
+
+    const tmp = dayjs(
+      [dayString, timeStringWithoutAmPm].join(" "),
+      // "MMMM D, YYYY H:mma", // see workaround
+      "MMMM D, YYYY H:mm",
+      true
+    );
+
+    let time = dayjs.tz(tmp.format("YYYY-MM-DD HH:mm"), "America/Los_Angeles");
+
+    const showIsWithinRange =
+      time.subtract(30, "minutes") > dayjs().subtract(25, "minutes") &&
+      time.subtract(30, "minutes") < dayjs().add(25, "minutes");
+
+    return showIsWithinRange;
+  });
+
+  if (!currentShowIssue) {
+    core.setFailed("No current issue found to comment on");
+    process.exit(1);
   }
-);
-console.log("Comment created at %s", commentUrl);
 
-// Tweet out that the show is live:
-const auth = {
-  consumerKey: process.env.TWITTER_CONSUMER_KEY,
-  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-  accessTokenKey: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-};
+  const [, , title, , guest] = currentShowIssue.title.split(/ (- |with @)/g);
 
-const tweetText = `📯  Starting in 30 minutes
+  const currentShow = {
+    title,
+    number: currentShowIssue.number,
+    issue: currentShowIssue,
+    guest,
+    url: currentShowIssue.html_url,
+  };
+
+  // add comment on issue
+  const {
+    data: { html_url: commentUrl },
+  } = await octokit.request(
+    "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
+    {
+      owner: "gr2m",
+      repo: "helpdesk",
+      issue_number: currentShow.number,
+      body: "Going live in 30 minutes at https://twitch.tv/gregorcodes",
+    }
+  );
+  core.info(`Comment created at ${commentUrl}`);
+
+  // Tweet out that the show is live:
+  const auth = {
+    consumerKey: env.TWITTER_CONSUMER_KEY,
+    consumerSecret: env.TWITTER_CONSUMER_SECRET,
+    accessTokenKey: env.TWITTER_ACCESS_TOKEN_KEY,
+    accessTokenSecret: env.TWITTER_ACCESS_TOKEN_SECRET,
+  };
+
+  const tweetText = `📯  Starting in 30 minutes
 
 💁🏻‍♂️  ${currentShow.title}
 🔴  Watch live at https://twitch.tv/gregorcodes
 
 ${currentShow.url}`;
 
-const data = await twitterRequest(`POST statuses/update.json`, {
-  auth,
-  status: tweetText,
-});
-const tweetUrl = `https://twitter.com/gr2m/status/${data.id_str}`;
+  const data = await twitterRequest(`POST statuses/update.json`, {
+    auth,
+    status: tweetText,
+  });
+  const tweetUrl = `https://twitter.com/gr2m/status/${data.id_str}`;
 
-console.log("Tweeted at %s", tweetUrl);
+  core.info(`Tweeted at ${tweetUrl}`);
 
-// update TODOs in issue
-await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
-  owner: "gr2m",
-  repo: "helpdesk",
-  issue_number: currentShow.number,
-  body: currentShow.issue.body
-    .replace(
-      /- \[ \] <!-- todo:announcement-tweet --> ([^\n]+)/,
-      `- [x] <!-- todo:announcement-tweet --> $1 (${tweetUrl})`
-    )
-    .replace(
-      /- \[ \] <!-- todo:announcement-issue-comment --> ([^\n]+)/,
-      `- [x] <!-- todo:announcement-issue-comment --> $1 (${commentUrl})`
-    ),
-});
+  // update TODOs in issue
+  await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
+    owner: "gr2m",
+    repo: "helpdesk",
+    issue_number: currentShow.number,
+    body: currentShow.issue.body
+      .replace(
+        /- \[ \] <!-- todo:announcement-tweet --> ([^\n]+)/,
+        `- [x] <!-- todo:announcement-tweet --> $1 (${tweetUrl})`
+      )
+      .replace(
+        /- \[ \] <!-- todo:announcement-issue-comment --> ([^\n]+)/,
+        `- [x] <!-- todo:announcement-issue-comment --> $1 (${commentUrl})`
+      ),
+  });
 
-console.log("TODOs in issue updated: %s", currentShow.url);
+  core.info(`TODOs in issue updated: ${currentShow.url}`);
+}
---
+++ test/handle-show-announcement-test.js
@@ -0,0 +1,138 @@
+import { readFile } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+import { deepEqual } from "assert/strict";
+
+import { test } from "uvu";
+import MockDate from "mockdate";
+
+import { run } from "../handle-show-announcement.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+test("handle-show-announcement.js", async () => {
+  const issues = JSON.parse(
+    await readFile(join(__dirname, "fixtures/list-issues.json"))
+  );
+
+  // mock date (30 minutes before the show)
+  MockDate.set("2021-08-19T16:30:00.000Z");
+
+  // mock environment variables
+  const mockEnv = {
+    TWITTER_CONSUMER_KEY: "twitter_consumer_key",
+    TWITTER_CONSUMER_SECRET: "twitter_consumer_secret",
+    TWITTER_ACCESS_TOKEN_KEY: "twitter_access_token_key",
+    TWITTER_ACCESS_TOKEN_SECRET: "twitter_access_token_secret",
+  };
+
+  // mock octokit
+  const mockOctokit = {
+    paginate: async (route, parameters) => {
+      if (route === "GET /repos/{owner}/{repo}/issues") {
+        deepEqual(parameters, {
+          owner: "gr2m",
+          repo: "helpdesk",
+          labels: "show",
+          state: "open",
+          per_page: 100,
+        });
+        return issues;
+      }
+
+      throw new Error("Unexpected route: " + route);
+    },
+
+    request: async (route, parameters) => {
+      if (
+        route === "POST /repos/{owner}/{repo}/issues/{issue_number}/comments"
+      ) {
+        deepEqual(parameters, {
+          owner: "gr2m",
+          repo: "helpdesk",
+          issue_number: 1,
+          body: "Going live in 30 minutes at https://twitch.tv/gregorcodes",
+        });
+
+        return {
+          data: {
+            html_url: "<new comment url>",
+          },
+        };
+      }
+
+      if (route === `PATCH /repos/{owner}/{repo}/issues/{issue_number}`) {
+        const body = `📅 Thursday, August 19, 2021
+🕐 10:00am Pacific Time
+- [x] <!-- todo:announcement-tweet --> 30 minute announcement tweet (https://twitter.com/gr2m/status/<twitter id_str>)
+- [x] <!-- todo:announcement-issue-comment --> 30 minute announcement comment (<new comment url>)
+`;
+
+        deepEqual(parameters.body, body);
+
+        deepEqual(parameters, {
+          owner: "gr2m",
+          repo: "helpdesk",
+          issue_number: 1,
+          body,
+        });
+        return;
+      }
+
+      throw new Error("Unexpected route: " + route);
+    },
+  };
+
+  // mock twitterRequest
+  const mockTwitterRequest = (route, parameters) => {
+    if (route === "POST statuses/update.json") {
+      deepEqual(parameters, {
+        auth: {
+          accessTokenKey: "twitter_access_token_key",
+          accessTokenSecret: "twitter_access_token_secret",
+          consumerKey: "twitter_consumer_key",
+          consumerSecret: "twitter_consumer_secret",
+        },
+        status: `📯  Starting in 30 minutes
+
+💁🏻‍♂️  Creating tests
+🔴  Watch live at https://twitch.tv/gregorcodes
+
+<show url #1>`,
+      });
+
+      return {
+        id_str: "<twitter id_str>",
+      };
+    }
+    throw new Error("Unexpected route: " + route);
+  };
+
+  // mock core
+  const outputLogs = [];
+  const outputs = {};
+  const mockCore = {
+    info(message) {
+      outputLogs.push(message);
+    },
+    setOutput(name, value) {
+      outputs[name] = value;
+    },
+    setFailed(message) {
+      throw new Error(message);
+    },
+  };
+
+  // run action
+  await run(mockEnv, mockCore, mockOctokit, mockTwitterRequest);
+
+  // assertions
+  deepEqual(outputLogs, [
+    "Comment created at <new comment url>",
+    "Tweeted at https://twitter.com/gr2m/status/<twitter id_str>",
+    "TODOs in issue updated: <show url #1>",
+  ]);
+});
+
+test.run();
