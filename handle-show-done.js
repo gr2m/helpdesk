d3f38b37a5a4f8b4343ae5c00cf8154b037c2f31
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-09-02 10:31:15 -0700

    test: handle-show-done.js

--- handle-show-done.js
+++ handle-show-done.js
@@ -14,73 +14,77 @@ dayjs.extend(customParseFormat);
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-const event = JSON.parse(await readFile(process.env.GITHUB_EVENT_PATH));
-
-// Create Octokit constructor with .paginate API and custom user agent
-const MyOctokit = Octokit.plugin(paginateRest).defaults({
-  userAgent: "gr2m-helpdesk",
-});
-const octokit = new MyOctokit({
-  auth: process.env.GITHUB_TOKEN,
-});
-
-const currentShowIssue = event.issue;
-
-if (!currentShowIssue) {
-  core.setFailed("No current issue found to comment on");
-  process.exit(1);
+if (process.env.GITHUB_ACTIONS && process.env.NODE_ENV !== "test") {
+  // Create Octokit constructor with .paginate API and custom user agent
+  const MyOctokit = Octokit.plugin(paginateRest).defaults({
+    userAgent: "gr2m-helpdesk",
+  });
+  const octokit = new MyOctokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+  run(process.env, core, octokit, twitterRequest);
 }
 
-const [, , title, , guest] = currentShowIssue.title.split(/ (- |with @)/g);
+export async function run(env, core, octokit, twitterRequest) {
+  const event = JSON.parse(await readFile(env.GITHUB_EVENT_PATH));
+  const currentShowIssue = event.issue;
 
-const currentShow = {
-  title,
-  number: currentShowIssue.number,
-  issue: currentShowIssue,
-  guest,
-  url: currentShowIssue.html_url,
-};
+  if (!currentShowIssue) {
+    core.setFailed("No current issue found to comment on");
+    process.exit(1);
+  }
 
-// add comment on issue
-const {
-  data: { html_url: commentUrl },
-} = await octokit.request(
-  "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
-  {
+  const [, , title, , guest] = currentShowIssue.title.split(/ (- |with @)/g);
+
+  const currentShow = {
+    title,
+    number: currentShowIssue.number,
+    issue: currentShowIssue,
+    guest,
+    url: currentShowIssue.html_url,
+  };
+
+  // add comment on issue
+  const {
+    data: { html_url: commentUrl },
+  } = await octokit.request(
+    "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
+    {
+      owner: "gr2m",
+      repo: "helpdesk",
+      issue_number: currentShow.number,
+      body: "Show is done for today, thank you all! Recording is coming up in a moment",
+    }
+  );
+  core.info(`Comment created at ${commentUrl}`);
+
+  // update twitter profile
+  // https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile
+  const auth = {
+    consumerKey: env.TWITTER_CONSUMER_KEY,
+    consumerSecret: env.TWITTER_CONSUMER_SECRET,
+    accessTokenKey: env.TWITTER_ACCESS_TOKEN_KEY,
+    accessTokenSecret: env.TWITTER_ACCESS_TOKEN_SECRET,
+  };
+
+  await twitterRequest(`POST account/update_profile.json`, {
+    auth,
+    name: "Gregor",
+    url: "https://github.com/gr2m/",
+  });
+
+  core.info("Twitter profile reverted to default");
+
+  // update TODOs in issue
+  await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
     owner: "gr2m",
     repo: "helpdesk",
     issue_number: currentShow.number,
-    body: "Show is done for today, thank you all! Recording is coming up in a moment",
-  }
-);
-console.log("Comment created at %s", commentUrl);
+    body: currentShow.issue.body.replace(
+      /- \[ \] <!-- todo:twitter-profile-reset --> ([^\n]+)/,
+      "- [x] <!-- todo:twitter-profile-reset --> $1 (https://twitter.com/gr2m)"
+    ),
+  });
 
-// update twitter profile
-// https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/manage-account-settings/api-reference/post-account-update_profile
-const auth = {
-  consumerKey: process.env.TWITTER_CONSUMER_KEY,
-  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-  accessTokenKey: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-};
-
-await twitterRequest(`POST account/update_profile.json`, {
-  auth,
-  name: "Gregor",
-  url: "https://github.com/gr2m/",
-});
-
-console.log("Twitter profile reverted to default");
-
-// update TODOs in issue
-await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
-  owner: "gr2m",
-  repo: "helpdesk",
-  issue_number: currentShow.number,
-  body: currentShow.issue.body.replace(
-    /- \[ \] <!-- todo:twitter-profile-reset --> ([^\n]+)/,
-    "- [x] <!-- todo:twitter-profile-reset --> $1 (https://twitter.com/gr2m)"
-  ),
-});
-
-console.log("TODOs in issue updated: %s", currentShow.url);
+  core.info(`TODOs in issue updated: ${currentShow.url}`);
+}
---
+++ test/handle-show-done-test.js
@@ -0,0 +1,98 @@
+import { readFile } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+import { deepEqual } from "assert/strict";
+
+import { test } from "uvu";
+
+import { run } from "../handle-show-done.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+test("handle-show-done.js", async () => {
+  // mock environment variables
+  const mockEnv = {
+    GITHUB_EVENT_PATH: join(__dirname, "fixtures/issues.closed.json"),
+    TWITTER_CONSUMER_KEY: "twitter_consumer_key",
+    TWITTER_CONSUMER_SECRET: "twitter_consumer_secret",
+    TWITTER_ACCESS_TOKEN_KEY: "twitter_access_token_key",
+    TWITTER_ACCESS_TOKEN_SECRET: "twitter_access_token_secret",
+  };
+
+  // mock octokit
+  const mockOctokit = {
+    request: async (route, parameters) => {
+      if (
+        route === "POST /repos/{owner}/{repo}/issues/{issue_number}/comments"
+      ) {
+        deepEqual(parameters, {
+          owner: "gr2m",
+          repo: "helpdesk",
+          issue_number: 1,
+          body: "Show is done for today, thank you all! Recording is coming up in a moment",
+        });
+        return {
+          data: { html_url: "<comment url>" },
+        };
+      }
+
+      if (route === "PATCH /repos/{owner}/{repo}/issues/{issue_number}") {
+        deepEqual(parameters, {
+          body: "- [x] <!-- todo:twitter-profile-reset --> reset twitter profile (https://twitter.com/gr2m)\n",
+          issue_number: 1,
+          owner: "gr2m",
+          repo: "helpdesk",
+        });
+        return;
+      }
+
+      throw new Error("Unexpected route: " + route);
+    },
+  };
+
+  // mock twitterRequest
+  const mockTwitterRequest = (route, parameters) => {
+    if (route === "POST account/update_profile.json") {
+      deepEqual(parameters, {
+        auth: {
+          consumerKey: "twitter_consumer_key",
+          consumerSecret: "twitter_consumer_secret",
+          accessTokenKey: "twitter_access_token_key",
+          accessTokenSecret: "twitter_access_token_secret",
+        },
+        name: "Gregor",
+        url: "https://github.com/gr2m/",
+      });
+      return;
+    }
+    throw new Error("Unexpected route: " + route);
+  };
+
+  // mock core
+  const outputLogs = [];
+  const outputs = {};
+  const mockCore = {
+    info(message) {
+      outputLogs.push(message);
+    },
+    setOutput(name, value) {
+      outputs[name] = value;
+    },
+    setFailed(message) {
+      throw new Error(message);
+    },
+  };
+
+  // run action
+  await run(mockEnv, mockCore, mockOctokit, mockTwitterRequest);
+
+  // assertions
+  deepEqual(outputLogs, [
+    "Comment created at <comment url>",
+    "Twitter profile reverted to default",
+    "TODOs in issue updated: <issue url>",
+  ]);
+});
+
+test.run();
